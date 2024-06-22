f725aaec754d5d85ef16aba46fd4e01df1c09cf8
Hom Dahal <homdahal997@gmail.com>
2024-06-22 13:40:54 -0400

    Update user model to include name and isAdmin fields, and add password encryption functionality

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,44 @@
 const { Schema, model } = require('../config/db-connection');
+const bcrypt = require('bcryptjs');
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userSchema = Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    unique: true,
-    min: 5,
-  },
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = model('User', userSchema);
