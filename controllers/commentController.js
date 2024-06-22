49025e1c4ded5010f52982d28219e15820896335
Hom Dahal <homdahal997@gmail.com>
2024-06-22 17:56:00 -0400

    Add updateComment and deleteComment routes and functions

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -8,6 +8,8 @@ module.exports = {
     createComment,
     getComments,
     getCommentById,
+    updateComment,
+    deleteComment
 };
 
 async function createComment(req, res, next) {
@@ -60,5 +62,47 @@ async function getCommentById(req, res, next) {
         res.status(200).json(comment);
     } catch (err) {
         next(err);
+    }
+}
+
+// Update a single comment by ID
+async function updateComment(req, res) {
+    try {
+        const updatedComment = await Comment.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+
+        res.status(200).json(updatedComment);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+// Delete a single comment by ID
+async function deleteComment(req, res) {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            message: 'Invalid comment id',
+        });
+    }
+
+    try {
+        const comment = await Comment.findById(id);
+
+        if (!comment) {
+            return res.status(404).json({
+                message: 'comment not found',
+            });
+        }
+
+        await Comment.findByIdAndDelete(id);
+
+        res.status(200).json({
+            message: 'Successfully deleted the comment',
+        });
+    } catch (err) {
+        res.status(500).send(err);
     }
 }
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -6,5 +6,7 @@ const router = Router();
 router.post('/', commentsCtrl.createComment);
 router.get('/', commentsCtrl.getComments);
 router.get('/:id', commentsCtrl.getCommentById);
+router.put('/:id', commentsCtrl.updateComment);
+router.delete('/:id', commentsCtrl.deleteComment);
 
 module.exports = router;
